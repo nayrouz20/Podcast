c047ff3e64d3ffbbbe7a1a7b1bac9fbebfeae1ee
nayrouz20 <nayrouzweslati2002@gmail.com>
2024-01-01 13:44:53 +0100

    commit3

--- app/src/main/java/com/example/mypodcast/view/Home.java
+++ app/src/main/java/com/example/mypodcast/view/Home.java
@@ -25,15 +25,84 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         // Initialisation du bouton en associant la vue du layout avec la variable Java
         btnDomaine1 = (Button) findViewById(R.id.btnDomaine1);
-
+        btnDomaine2 = (Button) findViewById(R.id.btnDomaine2);
+        btnDomaine3 = (Button) findViewById(R.id.btnDomaine3);
+        btnDomaine4 = (Button) findViewById(R.id.btnDomaine4);
+        btnDomaine5 = (Button) findViewById(R.id.btnDomaine5);
+        btnDomaine6 = (Button) findViewById(R.id.btnDomaine6);
         // Configuration de l'écouteur de clic pour le bouton "Consultation"
         btnDomaine1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Création d'une nouvelle intention (Intent) pour démarrer MainActivity
+                // Création d'une nouvelle intention (Intent) pour démarrer l'activité home
                 Intent intent = new Intent(Home.this, Actuality.class);
 
-                // Démarrage de l'activité MainActivity
+                // Démarrage de l'activité home
+                startActivity(intent);
+
+                // Fermeture de l'activité actuelle (HomeActivity)
+                finish();
+            }
+        });
+        btnDomaine2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Création d'une nouvelle intention (Intent) pour démarrer l'activité home
+                Intent intent = new Intent(Home.this, Technology.class);
+
+                // Démarrage de l'activité home
+                startActivity(intent);
+
+                // Fermeture de l'activité actuelle (HomeActivity)
+                finish();
+            }
+        });
+        btnDomaine3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Création d'une nouvelle intention (Intent) pour démarrer l'activité home
+                Intent intent = new Intent(Home.this, History.class);
+
+                // Démarrage de l'activité home
+                startActivity(intent);
+
+                // Fermeture de l'activité actuelle (HomeActivity)
+                finish();
+            }
+        });
+        btnDomaine4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Création d'une nouvelle intention (Intent) pour démarrer l'activité home
+                Intent intent = new Intent(Home.this, Comedy.class);
+
+                // Démarrage de l'activité home
+                startActivity(intent);
+
+                // Fermeture de l'activité actuelle (HomeActivity)
+                finish();
+            }
+        });
+        btnDomaine5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Création d'une nouvelle intention (Intent) pour démarrer l'activité home
+                Intent intent = new Intent(Home.this, Politics.class);
+
+                // Démarrage de l'activité home
+                startActivity(intent);
+
+                // Fermeture de l'activité actuelle (HomeActivity)
+                finish();
+            }
+        });
+        btnDomaine6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Création d'une nouvelle intention (Intent) pour démarrer l'activité home
+                Intent intent = new Intent(Home.this, MentalHealth.class);
+
+                // Démarrage de l'activité home
                 startActivity(intent);
 
                 // Fermeture de l'activité actuelle (HomeActivity)
